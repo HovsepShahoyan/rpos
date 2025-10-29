2be40bdc4e7ceccd61c257b40288d28b07fdee14
Hovsep Shahoyan <shahoyanhovsep@gmail.com>
2025-10-29 12:33:49 +0400

    temporary fix import

--- django_backend/apps/core/protos/camera_pb2_grpc.py
+++ django_backend/apps/core/protos/camera_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import camera_pb2 as camera__pb2
+import apps.core.protos.camera_pb2 as camera__pb2
 
 GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
